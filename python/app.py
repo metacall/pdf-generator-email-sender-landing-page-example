6a4229c73cb5b9cc839797a2c5ada2a5cc445c1f
Unknown <jakash1997@gmail.com>
2019-08-19 22:22:50 +0530

    Push

--- python/app.py
+++ python/app.py
@@ -5,6 +5,4 @@ def make_pdf(name):
   pdf.add_page()
   pdf.set_font('Arial', 'B', 16)
   pdf.cell(40, 10, 'Hey There ' + name + " !")
-  print(pdf.output(dest= 'S'))
-
-make_pdf('Jeff')
+  return pdf.output(dest= 'S')
